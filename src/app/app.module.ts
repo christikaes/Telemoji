33c0e7166c43f1380b445e8054a1fff214a574bb
christikaes <christikaes@gmail.com>
2018-05-02 17:52:53 -0400

    [Part 2] Generate Components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { JoinComponent } from './join/join.component';
+import { GameComponent } from './game/game.component';
+import { FinaleComponent } from './finale/finale.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    JoinComponent,
+    GameComponent,
+    FinaleComponent
   ],
   imports: [
     BrowserModule
